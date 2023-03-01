9a073c776f290fe2fe98f9136ba8049e58d31721
stefanpython <stefanhtml@yahoo.com>
2023-03-01 18:31:44 +0200

    gh-pages

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
     <div className="card-footer">
-      <img src="./dist/images/twitter.png" alt="twitter" />
-      <img src="./dist/images/fb.png" alt="facebook" />
-      <img src="./dist/images/gh.png" alt="github" />
-      <img src="./dist/images/insta.png" alt="instagram" />
-      <img src="./dist/images/linked.png" alt="linkedin" />
+      <img src="images/twitter.png" alt="twitter" />
+      <img src="images/fb.png" alt="facebook" />
+      <img src="images/gh.png" alt="github" />
+      <img src="images/insta.png" alt="instagram" />
+      <img src="images/linked.png" alt="linkedin" />
     </div>
   );
 }
